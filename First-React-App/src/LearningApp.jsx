e69cba9efdb5d15f09223eb852f431ffacbea9bf
mkanias <131133498+mkanias@users.noreply.github.com>
2025-03-28 10:18:47 +1100

    Saved the initial practice learning code

---
+++ First-React-App/src/LearningApp.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const Card = ({ title }) => {
+  const [count, setCount] = useState(0);
+  const [hasLiked, setHasLiked] = useState(false);
+
+  useEffect(() => {
+    console.log(`${title} has been liked: ${hasLiked}`);
+  }, [hasLiked]);
+
+
+  return (
+    <div className='card' onClick={() => {setCount(count + 1)}}>
+      <h2>{title} <br /> {count || null}</h2>
+
+      <button onClick={() => {setHasLiked(!hasLiked)}}>
+        {hasLiked ? '❤️' : '🤍'}
+      </button>
+    </div>
+  )
+}
+
+
+const App = () => {
+
+  return (
+    <div className='card-container'>
+      <Card title="Happy Potter" />
+      <Card title="Star Wars" />
+      <Card title="Rush Hour" />
+    </div>
+  )
+}
+export default App
